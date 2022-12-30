7adefdd1ab96a8fc66c5411ecf603b4086209a19
LuhxEn <axngrsantos@gmail.com>
2022-12-30 11:48:00 +0800

    Change text buttons into icons

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,12 @@
 import React from "react"
+import projectIcon from '../images/list-box-outline.svg';
+import editIcon from '../images/edit-icon.svg';
+import deleteIcon from '../images/delete-icon.svg';
+import saveIcon from '../images/save-edit.svg';
 
 const Project = (props) =>{
   const [projectTitle, setProjectTitle] = React.useState({title: props.title})
+  const [isHover, setIsHover] = React.useState(false);
 
   const handleChange = (event) =>{
     const {name, type, value, checked} = event.target;
@@ -13,24 +18,36 @@ const Project = (props) =>{
     console.log(projectTitle);
   }
 
-  const styles = {
-    border: props.selected ? "2px solid black" : "none"
+  const handleMouseEnter = () =>{
+    setIsHover(true);
   }
 
+  const handleMouseLeave = () =>{
+    setIsHover(false);
+  }
+
+  const styles = {
+    backgroundColor: props.selected || isHover ? "#CCCCCC" : "white",
+  }
+
+
   return(
-    <div style={styles} onClick={props.toggleSelected} className="project-item">
-      {!props.currentlyEditing && <h2>{props.title}</h2>}
+    <div style={styles} onClick={props.toggleSelected} className="project-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {!props.currentlyEditing && <img className="project-icon" src={projectIcon} alt="" />}
+
+      {!props.currentlyEditing && <p className="project-title">{props.title}</p>}
       {props.currentlyEditing && 
         <input 
           type="text"
+          id="edit-title-input"
           name="title"
           value={projectTitle.title}
           onChange={handleChange}
         />}
       
-      {!props.currentlyEditing && <button onClick={props.setEditMode}>Edit</button>}
-      {props.currentlyEditing && <button onClick={() => props.updateProject(props.id, projectTitle.title)}>Update</button>}
-      <button onClick={props.deleteProject}>Delete</button>
+      {!props.currentlyEditing && <img onClick={props.setEditMode} src={editIcon} alt="" />}
+      {props.currentlyEditing && <img onClick={() => props.updateProject(props.id, projectTitle.title)} src={saveIcon} alt="" />}
+      <img onClick={props.deleteProject} src={deleteIcon} alt="" />
     </div>
   )
 }
