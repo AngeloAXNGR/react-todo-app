af7c4adc873f633160e20420f4b8f0e2347be541
LuhxEn <axngrsantos@gmail.com>
2022-12-30 20:04:33 +0800

    Add conditional rendering and synthetic events for when Tasks are in edit mode

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,42 @@
 import React from "react";
 
 const Task = (props) =>{
+  const [taskData, setTaskData] = React.useState({taskTitle: props.taskTitle, taskDueDate: props.dueDate})
+
+
+  const handleChange = (event) =>{
+    const {name, type, value, checked} = event.target;
+    
+    setTaskData(prevData => {
+      return {...prevData,
+        [name] : type === "checkbox" ? checked : value
+      }
+    })
+    console.log(taskData);
+  }
+
   return(
     <div className="task-item">
-      <h1>{props.taskTitle}</h1>
-      <button onClick={props.deleteTask}>Delete</button>
+      <div  onClick={props.deleteTask} className="checkbox">
+      </div>
+      {!props.currentlyEditing && <p className="task-title" onClick={props.setTaskEditMode}>{props.taskTitle}</p>}
+      {props.currentlyEditing && 
+        <input 
+          type="text" 
+          name="taskTitle"
+          value={taskData.taskTitle}
+          onChange={handleChange} 
+          onBlur={() => props.updateTask(taskData.taskTitle, taskData.dueDate)}
+          />}
+      {!props.currentlyEditing && <p onClick={props.setTaskEditMode} className="task-duedate">{props.dueDate === ''  || props.dueDate === undefined ? "No Due Date" : `${props.dueDate}`}</p>}
+      {props.currentlyEditing && 
+        <input 
+          type="date" 
+          name="dueDate"
+          value={taskData.taskDueDate}
+          onChange={handleChange} 
+          onBlur={() => props.updateTask(taskData.taskTitle, taskData.dueDate)}
+          />}
     </div>
   );
 }
